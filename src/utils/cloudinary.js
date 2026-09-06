1719842f9288ee773ba840f4d7e798d4c35b4d5c
nn <mmoon8941@gmail.com>
2026-09-06 12:02:36 +0500

    configuration complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';
+import { log } from 'console';
+import fs from 'fs';
+
+cloudinary.config({
+    API_KEY: process.env.CL_API_KEY,
+    API_SECRET: process.env.CL_API_SECRET,
+    cloud_name: process.env.CL_PROJECT_NAME
+})
+
+const uploadImageCloudinary= async (filePath)=>{
+    try {
+        if(!filePath) return null;
+        const response= await cloudinary.uploader.upload(filePath,{
+            resource_type:"auto"
+        })
+        console.log("File uploaded successfully",response.url);
+        return response;
+        
+        
+    } catch (error) {
+        fs.unlinkSync(filePath) //remove locally save files only when it fails
+        return null
+        
+    }
+}
+
+
+
+
